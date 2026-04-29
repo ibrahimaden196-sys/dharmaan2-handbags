4eb7592db72eedf280afbbcedd614127a4c82902
ibrahimaden196-sys <ibrahimaden196@gmail.com>
2026-04-29 18:35:50 +0300

    Fix syntax error in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,7 @@ import { inspectAttr } from 'kimi-plugin-inspect-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
-  plugins: [inspectAttr(), react()],
+ [ plugins: [inspectAttr(), react()],
   server: {
     port: 3000,
   },
